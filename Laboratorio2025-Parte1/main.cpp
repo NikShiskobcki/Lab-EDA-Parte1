fe7e22f69fa31a10b12f30b85477e9a81be87f81
NikShiskobcki <87676940+NikShiskobcki@users.noreply.github.com>
2025-10-07 18:50:43 -0300

    Update main.cpp

--- Laboratorio2025-Parte1/main.cpp
+++ Laboratorio2025-Parte1/main.cpp
@@ -5,10 +5,21 @@
 using namespace std;
 
 int main (int argc, char *argv[]) {
-	Archivo a = CrearArchivo("curriculo.txt");
-	cout << a->titulo;
-	
-	
-	return 0;
-}
+    Archivo a;
+    a = CrearArchivo("curriculo.txt");
+    cout << a->titulo << endl;
+    cout << "Version: "<< a->version << endl;
 
+    InsertarLinea(a, "1", "Direccion: Rivera 1234", 1);
+    InsertarLinea(a, "1", "Telefono: 6111111", 2);
+    InsertarLinea(a, "1", "Nombre: Juan Perez", 1);
+
+	//mostrarTexto
+    Linea aux = a->contenido;
+    while (aux != NULL) {
+        cout << aux->nroLinea << '\t' << aux->texto << endl;
+        aux = aux->sig;
+    }
+
+    return 0;
+}
