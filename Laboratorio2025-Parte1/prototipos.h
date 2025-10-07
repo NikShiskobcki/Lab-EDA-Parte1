9f09bd40a3dd4bdfd49d6b25f017160837fd2ee3
NikShiskobcki <87676940+NikShiskobcki@users.noreply.github.com>
2025-10-07 18:51:00 -0300

    Update prototipos.h

--- Laboratorio2025-Parte1/prototipos.h
+++ Laboratorio2025-Parte1/prototipos.h
@@ -3,5 +3,8 @@
 
 Archivo CrearArchivo(char * nombre);
 TipoRet BorrarVersion(Archivo &a, char * version);
+TipoRet InsertarLinea(Archivo &a, char * version, char * linea, unsigned int nroLinea);
+TipoRet BorrarLinea(Archivo &a, char * version, unsigned int nroLinea);
+//mostrarTexto
 
 #endif
