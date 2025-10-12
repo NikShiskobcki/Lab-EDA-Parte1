f6d9a1fcc2d049bbf44d55fa700eef544a9d8ef0
NikShiskobcki <87676940+NikShiskobcki@users.noreply.github.com>
2025-10-12 15:50:48 -0300

    Update main.cpp

--- Laboratorio2025-Parte1/main.cpp
+++ Laboratorio2025-Parte1/main.cpp
@@ -1,25 +1,90 @@
+#include <cstring>
 #include<iostream>
-
 #include "estructuras.h"
 #include "prototipos.h"
 using namespace std;
 
 int main (int argc, char *argv[]) {
-    Archivo a;
-    a = CrearArchivo("curriculo.txt");
-    cout << a->titulo << endl;
-    cout << "Version: "<< a->version << endl;
+    Archivo a = NULL;
 
-    InsertarLinea(a, "1", "Direccion: Rivera 1234", 1);
-    InsertarLinea(a, "1", "Telefono: 6111111", 2);
-    InsertarLinea(a, "1", "Nombre: Juan Perez", 1);
+    Cadena comando = new char[100];
+    Cadena parametros[5];
 
-	//mostrarTexto
-    Linea aux = a->contenido;
-    while (aux != NULL) {
-        cout << aux->nroLinea << '\t' << aux->texto << endl;
-        aux = aux->sig;
+
+    while (strlen(comando)==0) {
+        cout << ">";
+        cin.getline(comando,100);
     }
 
+    char delim[] = "(,)";
+
+    int contador = 0;
+    parametros[contador] = strtok(comando,delim);
+
+    while (parametros[contador]!=NULL && contador<5) {
+        contador++;
+        parametros[contador] = strtok(NULL,delim);
+    }
+
+    opciones opcion;
+    while (strcmp(parametros[0],"salir")!=0) {
+        opcion = error;
+        if (strcmp(parametros[0],"crearA")==0) {opcion = crearA;}
+        if (strcmp(parametros[0],"borrarA")==0) {opcion = borrarA;}
+        if (strcmp(parametros[0],"insertar")==0) {opcion=insertar;}
+        if (strcmp(parametros[0],"borrarL")==0) {opcion=borrarL;}
+        if (strcmp(parametros[0],"mostrarT")==0) {opcion=mostrarT;}
+        if (strcmp(parametros[0],"ayuda")==0) {opcion=ayuda;}
+        if (strcmp(parametros[0],"limpiar")==0) {opcion=limpiar;}
+
+        switch (opcion) {
+            case crearA:
+                muestroRetorno(creoA(a));
+            break;
+
+            case borrarA:
+                muestroRetorno(borroA(a));
+            break;
+
+            case insertar:
+                cout << contador << endl;
+                muestroRetorno(inserto(a, parametros));
+            break;
+
+            case borrarL:
+                muestroRetorno(borroL(a,parametros));
+            break;
+
+            case mostrarT:
+                muestroRetorno(muestroT(a,parametros));
+            break;
+
+            case ayuda:
+                help();
+            break;
+
+            case limpiar:
+                system("cls");
+            break;
+
+            default:
+                muestroRetorno(ERROR);
+        }
+        fflush(stdin);
+        strcpy(comando,"");
+
+        while (strlen(comando)==0) {
+            cout << ">";
+            cin.getline(comando,100);
+        }
+
+        int contador = 0;
+        parametros[contador] = strtok(comando,delim);
+
+        while (parametros[contador]!=NULL && contador<5) {
+            contador++;
+            parametros[contador] = strtok(NULL,delim);
+        }
+    }
     return 0;
 }
