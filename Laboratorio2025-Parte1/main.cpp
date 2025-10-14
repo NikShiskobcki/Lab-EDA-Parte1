017ede265fb4d8cac3d6ad1581f13cf11d0d8441
NikShiskobcki <87676940+NikShiskobcki@users.noreply.github.com>
2025-10-14 08:56:17 -0300

    Update main.cpp

--- Laboratorio2025-Parte1/main.cpp
+++ Laboratorio2025-Parte1/main.cpp
@@ -1,90 +1,82 @@
 #include <cstring>
 #include<iostream>
+#include <stdio_ext.h>
 #include "estructuras.h"
 #include "prototipos.h"
 using namespace std;
 
 int main (int argc, char *argv[]) {
-    Archivo a = NULL;
-
-    Cadena comando = new char[100];
-    Cadena parametros[5];
-
-
-    while (strlen(comando)==0) {
-        cout << ">";
-        cin.getline(comando,100);
-    }
-
-    char delim[] = "(,)";
-
-    int contador = 0;
-    parametros[contador] = strtok(comando,delim);
-
-    while (parametros[contador]!=NULL && contador<5) {
-        contador++;
-        parametros[contador] = strtok(NULL,delim);
-    }
-
-    opciones opcion;
-    while (strcmp(parametros[0],"salir")!=0) {
-        opcion = error;
-        if (strcmp(parametros[0],"crearA")==0) {opcion = crearA;}
-        if (strcmp(parametros[0],"borrarA")==0) {opcion = borrarA;}
-        if (strcmp(parametros[0],"insertar")==0) {opcion=insertar;}
-        if (strcmp(parametros[0],"borrarL")==0) {opcion=borrarL;}
-        if (strcmp(parametros[0],"mostrarT")==0) {opcion=mostrarT;}
-        if (strcmp(parametros[0],"ayuda")==0) {opcion=ayuda;}
-        if (strcmp(parametros[0],"limpiar")==0) {opcion=limpiar;}
-
-        switch (opcion) {
-            case crearA:
-                muestroRetorno(creoA(a));
-            break;
-
-            case borrarA:
-                muestroRetorno(borroA(a));
-            break;
-
-            case insertar:
-                cout << contador << endl;
-                muestroRetorno(inserto(a, parametros));
-            break;
-
-            case borrarL:
-                muestroRetorno(borroL(a,parametros));
-            break;
-
-            case mostrarT:
-                muestroRetorno(muestroT(a,parametros));
-            break;
-
-            case ayuda:
-                help();
-            break;
-
-            case limpiar:
-                system("cls");
-            break;
-
-            default:
-                muestroRetorno(ERROR);
-        }
-        fflush(stdin);
-        strcpy(comando,"");
-
-        while (strlen(comando)==0) {
-            cout << ">";
-            cin.getline(comando,100);
-        }
-
-        int contador = 0;
-        parametros[contador] = strtok(comando,delim);
-
-        while (parametros[contador]!=NULL && contador<5) {
-            contador++;
-            parametros[contador] = strtok(NULL,delim);
-        }
-    }
-    return 0;
+	Archivo a = CrearArchivo("curriculum.txt");
+	
+	Cadena comando = new char[100];
+	Cadena parametros[5];
+	
+	
+	while (strlen(comando)==0) {
+		cout << ">";
+		cin.getline(comando,100);
+	}
+	
+	char delim[] = "(\",\")";
+	
+	int contador = 0;
+	parametros[contador] = strtok(comando,delim);
+	
+	while (parametros[contador]!=NULL && contador<5) {
+		contador++;
+		parametros[contador] = strtok(NULL,delim);
+	}
+	
+	opciones opcion;
+	
+	while (strcmp(parametros[0],"salir")!=0) {
+		opcion = error;
+		if (strcmp(parametros[0],"InsertarLinea")==0) {opcion=insertar;}
+		if (strcmp(parametros[0],"BorrarLinea")==0) {opcion=borrarL;}
+		if (strcmp(parametros[0],"MostrarTexto")==0) {opcion=mostrarT;}
+		if (strcmp(parametros[0],"ayuda")==0) {opcion=ayuda;}
+		if (strcmp(parametros[0],"limpiar")==0) {opcion=limpiar;}
+		
+		switch (opcion) {
+		case insertar:
+			muestroRetorno(inserto(a, parametros));
+			break;
+			
+		case borrarL:
+			muestroRetorno(borroL(a,parametros));
+			break;
+			
+		case mostrarT:
+			muestroRetorno(muestroT(a,parametros));
+			break;
+			
+		case ayuda:
+			help();
+			break;
+			
+		case limpiar:
+			system("clear");
+			break;
+			
+		default:
+			muestroRetorno(ERROR);
+		}
+		__fpurge(stdin);
+		strcpy(comando,"");
+		
+		while (strlen(comando)==0) {
+			cout << ">";
+			cin.getline(comando,100);
+		}
+		
+		int contador = 0;
+		parametros[contador] = strtok(comando,delim);
+		
+		while (parametros[contador]!=NULL && contador<5) {
+			contador++;
+			parametros[contador] = strtok(NULL,delim);
+		}
+	}
+	BorrarArchivo(a);
+	return 0;
 }
