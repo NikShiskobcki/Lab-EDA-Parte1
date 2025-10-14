e028309f6d0ba48072bd87abd2e813975b982358
NikShiskobcki <87676940+NikShiskobcki@users.noreply.github.com>
2025-10-14 08:58:33 -0300

    Update estructuras.h

--- Laboratorio2025-Parte1/estructuras.h
+++ Laboratorio2025-Parte1/estructuras.h
@@ -2,7 +2,7 @@
 #define ESTRUCTURAS_H
 
 enum _retorno{
-    OK, ERROR, NO_IMPLEMENTADA
+	OK, ERROR, NO_IMPLEMENTADA
 };
 typedef enum _retorno TipoRet;
 
@@ -10,23 +10,23 @@ typedef enum _retorno TipoRet;
 typedef char* Cadena;
 
 struct _linea {
-    unsigned int nroLinea;
-    Cadena texto = new char[100];
-    _linea* sig;
+	unsigned int nroLinea;
+	Cadena texto = new char[100];
+	_linea* sig;
 };
 
 typedef _linea* Linea;
 
 
 struct _archivo{
-    Cadena titulo;
-    Cadena version;
-    _linea* contenido;
-    _archivo* sig;
+	Cadena titulo;
+	Cadena version;
+	_linea* contenido;
+	_archivo* sig;
 };
 
 typedef enum _opciones{
-    crearA, borrarA, insertar, borrarL, mostrarT, ayuda, error, limpiar, salir
+	insertar, borrarL, mostrarT, ayuda, error, limpiar, salir
 }opciones;
 
 
