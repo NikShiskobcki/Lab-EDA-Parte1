6bd914e4323f1899bb81fccfa74b473d6f138642
NikShiskobcki <87676940+NikShiskobcki@users.noreply.github.com>
2025-10-14 08:58:04 -0300

    Update funcionesRetorno.cpp

--- Laboratorio2025-Parte1/funcionesRetorno.cpp
+++ Laboratorio2025-Parte1/funcionesRetorno.cpp
@@ -5,67 +5,55 @@ using namespace std;
 #include "prototipos.h"
 
 
-
-TipoRet creoA(Archivo &a) {
-    a = CrearArchivo("curriculo.txt");
-    return OK;
-}
-
-TipoRet borroA(Archivo &a) {
-    if (a==NULL)
-        return ERROR;
-    return BorrarArchivo(a);
-}
-
 TipoRet inserto(Archivo &a, Cadena parametros[]) {
-    if (a==NULL)
-        return ERROR;
-
-    Cadena version = new char[100];
-    strcpy(version, parametros[1]);
-
-    Cadena linea= new char[100];
-    strcpy(linea, parametros[2]);
-
-    unsigned int nroLinea = atoi(parametros[3]);
-
-    return InsertarLinea(a,version,linea,nroLinea);
+	if (a==NULL)
+		return ERROR;
+	
+	Cadena version = new char[100];
+	strcpy(version, parametros[2]);
+	
+	Cadena linea= new char[100];
+	strcpy(linea, parametros[3]);
+	
+	unsigned int nroLinea = atoi(parametros[4]);
+	
+	return InsertarLinea(a,version,linea,nroLinea);
 }
 
 TipoRet borroL(Archivo &a, Cadena parametros[]) {
-    if (a==NULL)
-        return ERROR;
-    Cadena version = new char[100];
-    strcpy(version, parametros[1]);
-
-
-    unsigned int nroLinea = atoi(parametros[2]);
-
-    return BorrarLinea(a, version, nroLinea);
+	if (a==NULL)
+		return ERROR;
+	Cadena version = new char[100];
+	strcpy(version, parametros[2]);
+	
+	
+	unsigned int nroLinea = atoi(parametros[3]);
+	
+	return BorrarLinea(a, version, nroLinea);
 }
 
 TipoRet muestroT(Archivo a, Cadena parametros[]) {
-    if (a==NULL) {
-        cout << "No contiene lineas" << endl;
-        return OK;
-    }
-    if (parametros[1]==NULL) {
-        return ERROR;
-    }
-    Cadena version = new char[10];
-    strcpy(version, parametros[1]);
-    return MostrarTexto(a,version);
+	if (a->contenido==NULL) {
+		cout << "No contiene lineas" << endl;
+		return OK;
+	}
+	if (parametros[1]==NULL) {
+		return ERROR;
+	}
+	Cadena version = new char[10];
+	strcpy(version, parametros[2]);
+	return MostrarTexto(a,version);
 }
 
 void muestroRetorno(_retorno ret){
-    switch (ret){
-        case OK:
-            cout << "OK"<< endl;
-            break;
-        case ERROR:
-            cout << "ERROR" << endl;
-            break;
-        case NO_IMPLEMENTADA:
-            cout << "NO IMPLEMENTADA"<<endl;
-    }
+	switch (ret){
+	case OK:
+		cout << "OK"<< endl;
+		break;
+	case ERROR:
+		cout << "ERROR" << endl;
+		break;
+	case NO_IMPLEMENTADA:
+		cout << "NO IMPLEMENTADA"<<endl;
+	}
 }
